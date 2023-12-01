46f46d436de9f8cba03751bc2420efd3faf578da
Rick Mann (Riari) <3583774+Riari@users.noreply.github.com>
2023-12-01 17:49:36 +0000

    Improve day 01

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -4,19 +4,17 @@ use regex::Regex;
 
 lazy_static! {
     static ref RE_DIGITS: Regex = Regex::new(r"\d").unwrap();
-    static ref WORD_MAP: HashMap<String, u32> = {
-        let mut m = HashMap::new();
-        m.insert("one".to_string(), 1);
-        m.insert("two".to_string(), 2);
-        m.insert("three".to_string(), 3);
-        m.insert("four".to_string(), 4);
-        m.insert("five".to_string(), 5);
-        m.insert("six".to_string(), 6);
-        m.insert("seven".to_string(), 7);
-        m.insert("eight".to_string(), 8);
-        m.insert("nine".to_string(), 9);
-        m
-    };
+    static ref WORD_MAP: HashMap<String, u32> = [
+        ("one".to_string(), 1),
+        ("two".to_string(), 2),
+        ("three".to_string(), 3),
+        ("four".to_string(), 4),
+        ("five".to_string(), 5),
+        ("six".to_string(), 6),
+        ("seven".to_string(), 7),
+        ("eight".to_string(), 8),
+        ("nine".to_string(), 9)
+    ].iter().cloned().collect();
 }
 
 advent_of_code::solution!(1);
@@ -25,14 +23,11 @@ fn solve(input: &str, with_words: bool) -> Option<u32> {
     let mut values: Vec<u32> = Vec::new();
     for line in input.lines() {
         let mut digits: HashMap<usize, u32> = HashMap::new();
-        for m in RE_DIGITS.find_iter(line) {
-            digits.insert(m.start(), m.as_str().parse::<u32>().unwrap());
-        }
-
-        if digits.len() == 1 {
-            // Only one digit encountered, so it's the first and last
-            let (pos, digit) = digits.iter().last().unwrap();
-            digits.insert(*pos, *digit);
+        let mut matches = RE_DIGITS.find_iter(line);
+        if let Some(first) = matches.next() {
+            let last = matches.last().unwrap_or(first);
+            digits.insert(first.start(), first.as_str().parse::<u32>().unwrap());
+            digits.insert(last.start(), last.as_str().parse::<u32>().unwrap());
         }
 
         if with_words {
@@ -41,13 +36,10 @@ fn solve(input: &str, with_words: bool) -> Option<u32> {
                 let substring: String = chars.iter().collect();
                 
                 for (word, digit) in WORD_MAP.iter() {
-                    let position = substring.find(word);
-                    if position.is_none() {
-                        continue;
+                    if let Some(position) = substring.find(word) {
+                        digits.insert(i + position, *digit);
+                        match_count += 1;
                     }
-
-                    digits.insert(i + position.unwrap(), *digit);
-                    match_count += 1;
                 }
             }
 
